38dbe33cf3f2e94579f3b795d6ba9d0e91115623
Genjo <insidia93@gmail.com>
2017-06-27 13:17:31 +0300

    Начальная инициализация

---
+++ autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+function __autoload($class)
+{
+    if (file_exists(__DIR__ . '/Controllers/' . $class . '.php' )){
+        require __DIR__ . '/Controllers/' . $class . '.php';
+    }
+    else if(file_exists(__DIR__ . '/Classes/' . $class . '.php'))
+    {
+        require __DIR__ . '/Classes/' . $class . '.php';
+    }
+    else if(file_exists(__DIR__ . '/Models/' . $class . '.php'))
+    {
+        require __DIR__ . '/Classes/' . $class . '.php';
+    }
+}
